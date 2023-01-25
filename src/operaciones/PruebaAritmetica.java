5cd248ad9e30ddc71fd42726db613b7dc0782c25
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-25 17:17:06 -0500

    Creacion de metodo parte 2

---
+++ src/operaciones/PruebaAritmetica.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author lepo9
+ */
+public class PruebaAritmetica {
+    
+    public static void main(String[] args) {
+        Aritmetica aritmetica1 = new Aritmetica();
+        aritmetica1.a = 3;
+        aritmetica1.b = 2;
+        aritmetica1.sumar();
+        
+        int resultado = aritmetica1.sumarConRetorno();
+        System.out.println("resultado desde la clase de prueba = " + resultado);
+    }
+}
