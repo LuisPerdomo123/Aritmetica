9bed8fc9200c69550dc88db2e63356de43c1f539
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-25 16:55:56 -0500

    Creacion metodo

---
+++ src/operaciones/Aritmetica.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author lepo9
+ */
+public class Aritmetica {
+    //Atributos de la clase
+    
+    int a;//El valor primitivo por defoult es 0
+    int b;
+    
+    //Metodos
+    
+    public void sumar(){
+    
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    
+}
