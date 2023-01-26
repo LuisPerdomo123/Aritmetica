9e0dc4b108e044e9d0321a1142aaaca928b168a7
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-26 09:49:09 -0500

    Alcance de variables

--- src/operaciones/PruebaAritmetica.java
+++ src/operaciones/PruebaAritmetica.java
@@ -9,29 +9,43 @@ package operaciones;
  * @author lepo9
  */
 public class PruebaAritmetica {
-    
+
     public static void main(String[] args) {
-        Aritmetica aritmetica1 = new Aritmetica();
+
         //Manejo de contructores y sobrecarga
-        System.out.println("aritmetica1 a: "+aritmetica1.a);//Impresion de los valores por default
-        System.out.println("aritmetica1 b: "+aritmetica1.b);
-        
+        //Alcance de variables
+        //Variables locales
+        var a = 10;//Var solo puede utilizarse para definir variables locales
+        var b = 2;
+
+        miMetodo();//El metodo main solo puede llamar a otros metodos estaticos
+
+        Aritmetica aritmetica1 = new Aritmetica();
+        System.out.println("aritmetica1 a: " + aritmetica1.a);//Impresion de los valores por default
+        System.out.println("aritmetica1 b: " + aritmetica1.b);
+
         Aritmetica aritmetica2 = new Aritmetica(5, 8);
-        
-        System.out.println("aritmetica2 a: "+aritmetica2.a);//Impresion de los valores por default
-        System.out.println("aritmetica2 b: "+aritmetica2.b);
-        
+
+        System.out.println("aritmetica2 a: " + aritmetica2.a);//Impresion de los valores por default
+        System.out.println("aritmetica2 b: " + aritmetica2.b);
+
         //Manejo de atributos, parametros y argumentos
         aritmetica1.a = 3;
         aritmetica1.b = 2;
         aritmetica1.sumar();
-        
+
         int resultado = aritmetica1.sumarConRetorno();
         System.out.println("resultado desde la clase de prueba = " + resultado);
-        
+
         resultado = aritmetica1.sumarConArgumentos(5, 8);//Envio de informacion se le llama parametro
         System.out.println("resultado usuando argumentos = " + resultado);
-        
-        
+
+    }
+
+    public static void miMetodo() {
+
+        //La variable a esta fuera del alcance donde fue definido
+        //a = 10;
+        System.out.println("otro metodo");
     }
 }
